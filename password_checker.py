f0964bf6ac262db6ec5d29107c946daa4260dc60
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-01-06 16:12:47 -0300

    Create password_checker.py

---
+++ password_checker.py
@@ -0,0 +1,23 @@
+def checkPasswordStrength(password):
+    while len(password) < 8:
+        print('Your password is too short!')
+        password = input('Type your password: ')
+
+    # Validation criteria & possible results
+    criteria = {
+        "lower": any(char.islower() for char in password),  
+        "upper": any(char.isupper() for char in password),  
+        "digit": any(char.isdigit() for char in password),  
+        "special": any(not char.isalnum() for char in password),
+    }
+    results = {
+        4: "Your password is excellent!",
+        3: "Your password is good!",
+        2: "Your password is weak.",
+        1: "Your password is very weak.",
+    }
+
+    # Do the math
+    points = sum(1 if met else 0 for met in criteria.values())
+
+    print(results.get(points))
