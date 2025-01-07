abdc096e5c177eae3c4bec6833b674614571d7e8
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-01-07 16:30:57 -0300

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,20 +1,20 @@
 def checkPasswordStrength(password):
-    while len(password) < 8:
-        print('Your password is too short!')
-        password = input('Type your password: ')
+    if len(password) < 8:
+        print('Your password is too short. Use another one')
+        return
 
     # Validation criteria & possible results
     criteria = {
-        "lower": any(char.islower() for char in password),  
-        "upper": any(char.isupper() for char in password),  
-        "digit": any(char.isdigit() for char in password),  
-        "special": any(not char.isalnum() for char in password),
+        'lower': any(char.islower() for char in password),  
+        'upper': any(char.isupper() for char in password),  
+        'digit': any(char.isdigit() for char in password),  
+        'special': any(not char.isalnum() for char in password),
     }
     results = {
-        4: "Your password is excellent!",
-        3: "Your password is good!",
-        2: "Your password is weak.",
-        1: "Your password is very weak.",
+        4: 'Your password is excellent!',
+        3: 'Your password is good!',
+        2: 'Your password is weak. Use another one',
+        1: 'Your password is very weak. Use another one',
     }
 
     # Do the math
@@ -25,4 +25,5 @@ def checkPasswordStrength(password):
 # Tests (not my passwords lol)
 checkPasswordStrength('Password02')
 checkPasswordStrength('!SkyFly250')
+checkPasswordStrength('1234567')
 checkPasswordStrength('12345678')
